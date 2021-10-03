fe96bcf02f2be3944e934a7ce173c52d746bdb2e
rsato1007 <robertmsato@gmail.com>
2021-10-03 10:16:00 -0600

    connecting to database

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
+from .models import Spices
 
 # Create your views here.
 
@@ -15,6 +16,10 @@ class About(TemplateView):
 class Index(TemplateView):
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["spices"] = Spices.objects.all() # Here we are using the model to query the database for us
+        return context
 # Saving this for reference
 
 # class ArtistList(TemplateView):
