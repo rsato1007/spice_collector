0e5b22f72fe9e17f2bf77d4f5847e08f2fd2c24a
rsato1007 <robertmsato@gmail.com>
2021-10-02 19:44:13 -0600

    adding extra pages and gitignore

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,9 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     # Main Page Path
-    path('', views.Home.as_view(), name="home")
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
+    path('index/', views.Index.as_view(), name="index")
     # Create New Spice Path
     # Individual Spice Route
     # Edit Spice Path
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,9 @@ from django.views.generic.base import TemplateView
 # This is what actual serves the home page template
 class Home(TemplateView):
     template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
+
+class Index(TemplateView):
+    template_name = "index.html"
