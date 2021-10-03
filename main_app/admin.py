e2bf744e90d792b00cf82ed57331435ed8a02e23
rsato1007 <robertmsato@gmail.com>
2021-10-03 10:02:16 -0600

    adding admin who can access models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Spices # import the Artist model from models.py
 # Register your models here.
+
+admin.site.register(Spices) # this line will add the model to the admin panel
