f3db2c1c609cab6ebf229ffa8f310b2ea9c8b61a
rsato1007 <robertmsato@gmail.com>
2021-10-04 18:00:14 -0600

    adding nav bar

--- main_app/views.py
+++ main_app/views.py
@@ -20,12 +20,3 @@ class Index(TemplateView):
         context = super().get_context_data(**kwargs)
         context["spices"] = Spices.objects.all() # Here we are using the model to query the database for us
         return context
-# Saving this for reference
-
-# class ArtistList(TemplateView):
-#     template_name = "artist_list.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context["artists"] = artists # this is where we add the key into our context object for the view to use
-#         return context
